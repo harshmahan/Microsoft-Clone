d1820f9262c9dbfec5671efe4943913872bd5d4b
harshmahan <75237220+harshmahan@users.noreply.github.com>
2022-03-23 23:22:35 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+// let ham = document.getElementsByClassName('hamburger')[0];
+
+// const hamburger = () => {
+
+// }
+
+// addEventListener('click', function(e) {
+//     if (e.target.className == 'hamburger') {
+//         e.target.classList.toggle('is-active');
+//         document.getElementById('nav-menu').classList.toggle('is-active');
+//     }
+// }
